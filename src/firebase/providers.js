7dd473858391679075ef427c0b071d320a15d8e4
Johan Restrepo <johanestebanrestrepo@gmail.com>
2022-11-22 15:55:51 -0500

    firebase providers implementados

---
+++ src/firebase/providers.js
@@ -0,0 +1,61 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth'
+import { FirebaseAuth } from './config'
+
+export const registerWithEmailAndPassword = async ({ email, password }) => {
+  try {
+    const response = await createUserWithEmailAndPassword(
+      FirebaseAuth,
+      email,
+      password
+    )
+    const { uid } = response.user
+    return {
+      ok: true,
+      uid,
+      email,
+    }
+  } catch (error) {
+    return {
+      ok: false,
+      msg: error.message,
+    }
+  }
+}
+
+export const loginWithEmailAndPassword = async ({ email, password }) => {
+  try {
+    const response = await signInWithEmailAndPassword(
+      FirebaseAuth,
+      email,
+      password
+    )
+    const { uid } = response.user
+    return {
+      ok: true,
+      uid,
+    }
+  } catch (error) {
+    return {
+      ok: false,
+      msg: error.message,
+    }
+  }
+}
+
+export const logoutFirebase = async () => {
+  try {
+    await signOut(FirebaseAuth)
+    return {
+      ok: true,
+    }
+  } catch (error) {
+    return {
+      ok: false,
+      msg: error.message,
+    }
+  }
+}
